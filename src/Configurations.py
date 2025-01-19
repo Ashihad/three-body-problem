8604473066a3c2d8f02b22370932dc4c4c26bbb7
Ashihad <grzegorz.esser@gmail.com>
2025-01-19 03:53:33 +0100

    Added docstrings

--- src/Configurations.py
+++ src/Configurations.py
@@ -1,6 +1,46 @@
+""" @package Configurations 
+
+@brief Available system configurations
+
+@details This module defines predefined configurations of various three body problems which can be selected from command line.
+Each function defined here returns a dictionary containing all data needed for differential equation solver
+to solve a given problem. Each dictionary **must** contain exactly 3 `ObjectParams2D` objects under `1`, `2` and `3`
+dictionary keys, `days` parameter, and `G` parameter. Other parameters are optional, but may disable some parts
+of the program (e.g. not specifying `frames` results in lack of animation generation).
+
+Parameters overview:
+- `1`/`2`/`3` - `ObjectParams2D`s containing initial conditions and masses of each body
+- `G` - value of gravitational constant, some configurations run with `G == 1`, some with real life value
+- `days` - upper bound of simulation time, it might be fractional (e.g. value 1/24 specifies one hour)
+- `frames` - total animation frames, this parameter is directly passed to `matplotlib.animation.FuncAnimation` handler, ommitting it disables animation generation
+- `title` - if set, plots will have this string displayed above them
+- `phase_detailed_x` - creating such dictionary implies zoomed phase plot generation for x/v_x parameters of specified body
+  - `body_no` chooses a body for zoomed plot, numbers `1`, `2` and `3` are only valid imputs
+  - `xrange` is a tuple of `(xrange_min, xragne_max)` for zoom plot
+  - `yrange` is a tuple of `(yrange_min, yragne_max)` for zoom plot
+- `lyapunov` - creating such dictionary implies generating a Lyapunov exponent plot for a given body, parameter, range of change and simulation time
+  - `body_no` chooses a body for zoomed plot, numbers `1`, `2` and `3` are only valid imputs
+  - `range` defines an iterable containing a range of change for a given parameter TODO this is hardcoded 
+  - `param` sets a label for x axis, this argument is passed directly to `matplotlib.pyplot`
+  - `days` specifies maximum simulation time for each Lyapunov exponent, it is usually shorter than normal simulation time
+
+Usage example:
+@code
+  # assuming user has chosen `sun_earth_mars` configuration
+  sun_earth_mars, plotting_params = ThreeBodyArgParser()
+
+  params = sun_earth_mars()
+  params = params | plot_params
+  simulator = ThreeBodySimulator(params)
+@endcode
+"""
+
 from .Utils import *
 
 def sun_earth_mars():
+  """Sun-Earth-Mars system
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 6.67430e-11
   params['1'] = ObjectParams2D(0, 0, 0, 0, 1.989e30)            # sun
@@ -12,6 +52,9 @@ def sun_earth_mars():
   return params
 
 def newton_problem():
+  """Sun-Earth-Moon system
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 6.67430e-11
   params['1'] = ObjectParams2D(0, 0, 0, 0, 1.989e30)                            # sun
@@ -23,6 +66,9 @@ def newton_problem():
   return params
 
 def triangle():
+  """Equilateral triangle
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 6.67430e-11
   params['1'] = ObjectParams2D(-np.sqrt(3)/2*1.5e11, -1/2*1.5e11, 0, 0, 1.989e32)
@@ -49,6 +95,10 @@ def rotating_triangle():
   return params
 
 def burrau():
+  """Burrau problem (3-4-5 triangle)
+  @see https://en.wikipedia.org/wiki/Three-body_problem#Special-case_solutions
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 6.67430e-11
   params['1'] = ObjectParams2D(0, 0, 0, 0, 5)
@@ -57,9 +107,25 @@ def burrau():
   params['days'] = 80
   params['frames'] = 500
   params['title'] = "Burrau problem"
+
+  params['phase_detailed_x'] = {}
+  params['phase_detailed_x']['body_no'] = 1
+  params['phase_detailed_x']['xrange'] = (-1.5, -0.6)
+  params['phase_detailed_x']['yrange'] = (-0.0001, 0.0001)
+
+  params['lyapunov'] = {}
+  params['lyapunov']['body_no'] = 2
+  params['lyapunov']['param'] = '$x_0$'
+  params['lyapunov']['range'] = np.linspace(params['2'].x_0, params['2'].x_0 + 2.999, 10)
+  params['lyapunov']['days'] = 20
+
   return params
 
 def burrau_shifted():
+  """Burrau problem (3-4-5 triangle), shifted by 1e-6 in various directions
+  @see https://en.wikipedia.org/wiki/Three-body_problem#Special-case_solutions
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 6.67430e-11
   params['1'] = ObjectParams2D(0.000001, -0.000001, 0, 0, 5)
@@ -68,9 +134,25 @@ def burrau_shifted():
   params['days'] = 80
   params['frames'] = 500
   params['title'] = "Burrau problem, shifted by 1e-6"
+
+  params['phase_detailed_x'] = {}
+  params['phase_detailed_x']['body_no'] = 1
+  params['phase_detailed_x']['xrange'] = (-1.5, -0.6)
+  params['phase_detailed_x']['yrange'] = (-0.0001, 0.0001)
+
+  params['lyapunov'] = {}
+  params['lyapunov']['body_no'] = 2
+  params['lyapunov']['param'] = '$x_0$'
+  params['lyapunov']['range'] = np.linspace(params['2'].x_0, params['2'].x_0 + 2.999, 10)
+  params['lyapunov']['days'] = 20
+
   return params
 
 def burrau_less_shifted():
+  """Burrau problem (3-4-5 triangle), shifted by 1e-12 in various directions
+  @see https://en.wikipedia.org/wiki/Three-body_problem#Special-case_solutions
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 6.67430e-11
   params['1'] = ObjectParams2D(0.000000000001, -0.000000000001, 0, 0, 5)
@@ -79,12 +161,27 @@ def burrau_less_shifted():
   params['days'] = 80
   params['frames'] = 500
   params['title'] = "Burrau problem, shifted by 1e-12"
+
+  params['phase_detailed_x'] = {}
+  params['phase_detailed_x']['body_no'] = 1
+  params['phase_detailed_x']['xrange'] = (-1.5, -0.6)
+  params['phase_detailed_x']['yrange'] = (-0.0001, 0.0001)
+
+  params['lyapunov'] = {}
+  params['lyapunov']['body_no'] = 2
+  params['lyapunov']['param'] = '$x_0$'
+  params['lyapunov']['range'] = np.linspace(params['2'].x_0, params['2'].x_0 + 2.999, 10)
+  params['lyapunov']['days'] = 20
+
   return params
 
 # https://arxiv.org/pdf/1705.00527
 
 def xiaoming_li_et_all_1():
-  # December 2017
+  """Example periodic configuration, from Xiaoming Li et all
+  @see https://arxiv.org/pdf/1705.00527
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 1
   v1 = 0.4159559963
@@ -98,7 +195,10 @@ def xiaoming_li_et_all_1():
   return params
 
 def xiaoming_li_et_all_2():
-  # December 2017
+  """Example periodic configuration, from Xiaoming Li et all
+  @see https://arxiv.org/pdf/1705.00527
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 1
   v1 = 0.3231926176
@@ -112,7 +212,10 @@ def xiaoming_li_et_all_2():
   return params
 
 def xiaoming_li_et_all_3():
-  # December 2017
+  """Example periodic configuration, from Xiaoming Li et all
+  @see https://arxiv.org/pdf/1705.00527
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 1
   v1 = 0.3369172422
@@ -126,6 +229,9 @@ def xiaoming_li_et_all_3():
   return params
 
 def l1():
+  """Lagrange L1 point for Sun-Earth system
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 6.67430e-11
   params['1'] = ObjectParams2D(0, 0, 0, 0, 1.989e30)                            # sun
@@ -143,8 +249,10 @@ def l1():
   return params
 
 def butterfly():
-  # Suvakov et all
-  # https://d1wqtxts1xzle7.cloudfront.net/35314308/PhysRevLett.110.114301-libre.pdf?1414532854=&response-content-disposition=inline%3B+filename%3DThree_Classes_of_Newtonian_Three_Body_Pl.pdf&Expires=1736637888&Signature=BgW9VEbj3h~LWtuJrAIY668dqaAEfmOBdN3HEMwa8lNN3O1OuoMMT0ckTebycVysRI6CZFML-8w~m8ilCH56MoKzU16rtac3EwFLKhc4u2Q2slc2CQ91-cxzlivugyPZwFlQ7teF6yI~xTnpah1QVQw7xaaYviMtNT0VISrmvkiN6rnub1rkw~KcWzM2undzMwdZ7EV7DmbzNOqzDI2Q68wGJVE2Cofj9R7g1-6-oRKfIZf~-QE3fRN9xwjz1jI~2VN7AoyQCWxSdmP8g6Ww8qWNTpCnOMWX7mYYroNeIGaplsNms5KndLI8LEpk1czsNo3jrLTRRummynCESgbSSA__&Key-Pair-Id=APKAJLOHF5GGSLRBV4ZA
+  """\"Butterfly\" configuration, from Suvakov et all
+  @see https://d1wqtxts1xzle7.cloudfront.net/35314308/PhysRevLett.110.114301-libre.pdf?1414532854=&response-content-disposition=inline%3B+filename%3DThree_Classes_of_Newtonian_Three_Body_Pl.pdf&Expires=1736637888&Signature=BgW9VEbj3h~LWtuJrAIY668dqaAEfmOBdN3HEMwa8lNN3O1OuoMMT0ckTebycVysRI6CZFML-8w~m8ilCH56MoKzU16rtac3EwFLKhc4u2Q2slc2CQ91-cxzlivugyPZwFlQ7teF6yI~xTnpah1QVQw7xaaYviMtNT0VISrmvkiN6rnub1rkw~KcWzM2undzMwdZ7EV7DmbzNOqzDI2Q68wGJVE2Cofj9R7g1-6-oRKfIZf~-QE3fRN9xwjz1jI~2VN7AoyQCWxSdmP8g6Ww8qWNTpCnOMWX7mYYroNeIGaplsNms5KndLI8LEpk1czsNo3jrLTRRummynCESgbSSA__&Key-Pair-Id=APKAJLOHF5GGSLRBV4ZA
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 1
   vx = 0.30689
@@ -158,8 +266,10 @@ def butterfly():
   return params
 
 def bumblebee():
-  # Suvakov et all
-  # https://d1wqtxts1xzle7.cloudfront.net/35314308/PhysRevLett.110.114301-libre.pdf?1414532854=&response-content-disposition=inline%3B+filename%3DThree_Classes_of_Newtonian_Three_Body_Pl.pdf&Expires=1736637888&Signature=BgW9VEbj3h~LWtuJrAIY668dqaAEfmOBdN3HEMwa8lNN3O1OuoMMT0ckTebycVysRI6CZFML-8w~m8ilCH56MoKzU16rtac3EwFLKhc4u2Q2slc2CQ91-cxzlivugyPZwFlQ7teF6yI~xTnpah1QVQw7xaaYviMtNT0VISrmvkiN6rnub1rkw~KcWzM2undzMwdZ7EV7DmbzNOqzDI2Q68wGJVE2Cofj9R7g1-6-oRKfIZf~-QE3fRN9xwjz1jI~2VN7AoyQCWxSdmP8g6Ww8qWNTpCnOMWX7mYYroNeIGaplsNms5KndLI8LEpk1czsNo3jrLTRRummynCESgbSSA__&Key-Pair-Id=APKAJLOHF5GGSLRBV4ZA
+  """\"Bumblebee\" configuration, from Suvakov et all
+  @see https://d1wqtxts1xzle7.cloudfront.net/35314308/PhysRevLett.110.114301-libre.pdf?1414532854=&response-content-disposition=inline%3B+filename%3DThree_Classes_of_Newtonian_Three_Body_Pl.pdf&Expires=1736637888&Signature=BgW9VEbj3h~LWtuJrAIY668dqaAEfmOBdN3HEMwa8lNN3O1OuoMMT0ckTebycVysRI6CZFML-8w~m8ilCH56MoKzU16rtac3EwFLKhc4u2Q2slc2CQ91-cxzlivugyPZwFlQ7teF6yI~xTnpah1QVQw7xaaYviMtNT0VISrmvkiN6rnub1rkw~KcWzM2undzMwdZ7EV7DmbzNOqzDI2Q68wGJVE2Cofj9R7g1-6-oRKfIZf~-QE3fRN9xwjz1jI~2VN7AoyQCWxSdmP8g6Ww8qWNTpCnOMWX7mYYroNeIGaplsNms5KndLI8LEpk1czsNo3jrLTRRummynCESgbSSA__&Key-Pair-Id=APKAJLOHF5GGSLRBV4ZA
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 1
   vx = 0.18428
@@ -173,8 +283,10 @@ def bumblebee():
   return params
 
 def goggles():
-  # Suvakov et all
-  # https://d1wqtxts1xzle7.cloudfront.net/35314308/PhysRevLett.110.114301-libre.pdf?1414532854=&response-content-disposition=inline%3B+filename%3DThree_Classes_of_Newtonian_Three_Body_Pl.pdf&Expires=1736637888&Signature=BgW9VEbj3h~LWtuJrAIY668dqaAEfmOBdN3HEMwa8lNN3O1OuoMMT0ckTebycVysRI6CZFML-8w~m8ilCH56MoKzU16rtac3EwFLKhc4u2Q2slc2CQ91-cxzlivugyPZwFlQ7teF6yI~xTnpah1QVQw7xaaYviMtNT0VISrmvkiN6rnub1rkw~KcWzM2undzMwdZ7EV7DmbzNOqzDI2Q68wGJVE2Cofj9R7g1-6-oRKfIZf~-QE3fRN9xwjz1jI~2VN7AoyQCWxSdmP8g6Ww8qWNTpCnOMWX7mYYroNeIGaplsNms5KndLI8LEpk1czsNo3jrLTRRummynCESgbSSA__&Key-Pair-Id=APKAJLOHF5GGSLRBV4ZA
+  """\"Goggles\" configuration, from Suvakov et all
+  @see https://d1wqtxts1xzle7.cloudfront.net/35314308/PhysRevLett.110.114301-libre.pdf?1414532854=&response-content-disposition=inline%3B+filename%3DThree_Classes_of_Newtonian_Three_Body_Pl.pdf&Expires=1736637888&Signature=BgW9VEbj3h~LWtuJrAIY668dqaAEfmOBdN3HEMwa8lNN3O1OuoMMT0ckTebycVysRI6CZFML-8w~m8ilCH56MoKzU16rtac3EwFLKhc4u2Q2slc2CQ91-cxzlivugyPZwFlQ7teF6yI~xTnpah1QVQw7xaaYviMtNT0VISrmvkiN6rnub1rkw~KcWzM2undzMwdZ7EV7DmbzNOqzDI2Q68wGJVE2Cofj9R7g1-6-oRKfIZf~-QE3fRN9xwjz1jI~2VN7AoyQCWxSdmP8g6Ww8qWNTpCnOMWX7mYYroNeIGaplsNms5KndLI8LEpk1czsNo3jrLTRRummynCESgbSSA__&Key-Pair-Id=APKAJLOHF5GGSLRBV4ZA
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 1
   vx = 0.08330
@@ -188,8 +300,10 @@ def goggles():
   return params
 
 def yinyang():
-  # Suvakov et all
-  # https://d1wqtxts1xzle7.cloudfront.net/35314308/PhysRevLett.110.114301-libre.pdf?1414532854=&response-content-disposition=inline%3B+filename%3DThree_Classes_of_Newtonian_Three_Body_Pl.pdf&Expires=1736637888&Signature=BgW9VEbj3h~LWtuJrAIY668dqaAEfmOBdN3HEMwa8lNN3O1OuoMMT0ckTebycVysRI6CZFML-8w~m8ilCH56MoKzU16rtac3EwFLKhc4u2Q2slc2CQ91-cxzlivugyPZwFlQ7teF6yI~xTnpah1QVQw7xaaYviMtNT0VISrmvkiN6rnub1rkw~KcWzM2undzMwdZ7EV7DmbzNOqzDI2Q68wGJVE2Cofj9R7g1-6-oRKfIZf~-QE3fRN9xwjz1jI~2VN7AoyQCWxSdmP8g6Ww8qWNTpCnOMWX7mYYroNeIGaplsNms5KndLI8LEpk1czsNo3jrLTRRummynCESgbSSA__&Key-Pair-Id=APKAJLOHF5GGSLRBV4ZA
+  """\"Yin-Yang\" configuration, from Suvakov et all
+  @see https://d1wqtxts1xzle7.cloudfront.net/35314308/PhysRevLett.110.114301-libre.pdf?1414532854=&response-content-disposition=inline%3B+filename%3DThree_Classes_of_Newtonian_Three_Body_Pl.pdf&Expires=1736637888&Signature=BgW9VEbj3h~LWtuJrAIY668dqaAEfmOBdN3HEMwa8lNN3O1OuoMMT0ckTebycVysRI6CZFML-8w~m8ilCH56MoKzU16rtac3EwFLKhc4u2Q2slc2CQ91-cxzlivugyPZwFlQ7teF6yI~xTnpah1QVQw7xaaYviMtNT0VISrmvkiN6rnub1rkw~KcWzM2undzMwdZ7EV7DmbzNOqzDI2Q68wGJVE2Cofj9R7g1-6-oRKfIZf~-QE3fRN9xwjz1jI~2VN7AoyQCWxSdmP8g6Ww8qWNTpCnOMWX7mYYroNeIGaplsNms5KndLI8LEpk1czsNo3jrLTRRummynCESgbSSA__&Key-Pair-Id=APKAJLOHF5GGSLRBV4ZA
+  @returns Dictionary with simulation parameters
+  """
   params = {}
   params['G'] = 1
   vx = 0.51394
--- src/Utils.py
+++ src/Utils.py
@@ -1,3 +1,32 @@
+"""This module defines a dataclass which holds initial conditions of a body in three body problem.
+
+`ObjectParams2D` objects are expected to be held inside a dictionary under keys `1`, `2` and `3`.
+Each aprameter is mandatory, they can be provided in every standard way supported by `dataclasses` module.
+
+Members overview:
+- `x_0` - specifies body's initial position in x axis
+- `y_0` - specifies body's initial position in y axis
+- `vx_0` - specifies body's initial velocity in x axis
+- `vy_0` - specifies body's initial velocity in y axis
+- `m` - specifies body's mass
+
+Usage example:
+@code
+  earth_x0 = 0
+  earth_y0 = 1.5e11
+  earth_vx0 = 29.78e3
+  earth_vy0 = 0
+  earth_mass = 5.97e24
+  earth_like_object = ObjectParams2D(
+    earth_x0,
+    earth_y0,
+    earth_vx0,
+    earth_vy0,
+    earth_mass
+  )
+@endcode
+"""
+
 from dataclasses import dataclass
 
 import numpy as np
